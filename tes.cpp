68f69939d48800fa8dc290a249825e4968fd55d2
Nayan-09 <116558820+Nayan-09@users.noreply.github.com>
2022-10-25 10:28:48 +0530

    Update tes.cpp
    So my problem after running the 1st loop the 2nd loop is run for 1 time again when 1st loop run then 2nd loop doesn't run.

--- tes.cpp
+++ tes.cpp
@@ -13,7 +13,7 @@ int main()
     i=1;
     while(i<=n)
     {
-        //printf("*****");
+        //printf("*****");          //So my problem after running the 1st loop the 2nd loop is run for 1 time again when 1st loop run then 2nd loop doesn't run.
         while(j<=5)
         {
             cout<<"*";
